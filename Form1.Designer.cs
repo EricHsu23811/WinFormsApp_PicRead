d7f3b9fe5c933264133f0f8fa95275a01651c18f
EricHsu_personal <99955365+EricHsu23811@users.noreply.github.com>
2025-09-15 14:51:36 +0800

    WinFormsApp_PicRead，讀取圖檔，使用 Tesseract OCR 引擎 進行文字辨識
    2025-09-15, 版本1.0.0.0： WinFormsApp_PicRead，讀取圖檔，使用 Tesseract OCR 引擎 進行文字辨識

---
+++ Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace WinFormsApp_PicRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtMac = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblSWver = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnImgRead = new System.Windows.Forms.Button();
+            this.FilePath1 = new System.Windows.Forms.TextBox();
+            this.btnImgSelect = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblOcrResult = new System.Windows.Forms.Label();
+            this.txtOcrResult = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 99;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(47, 26);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Enabled = false;
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(73, 26);
+            this.settingToolStripMenuItem.Text = "Setting";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 26);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(134, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 425);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 25);
+            this.statusStrip1.TabIndex = 100;
+            this.statusStrip1.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(81, 19);
+            this.toolStripStatusLabel1.Text = "Status text";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtMac
+            // 
+            this.txtMac.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMac.Enabled = false;
+            this.txtMac.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMac.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.txtMac.Location = new System.Drawing.Point(596, 34);
+            this.txtMac.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMac.Name = "txtMac";
+            this.txtMac.Size = new System.Drawing.Size(188, 21);
+            this.txtMac.TabIndex = 102;
+            this.txtMac.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label13.Location = new System.Drawing.Point(518, 33);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(73, 19);
+            this.label13.TabIndex = 103;
+            this.label13.Text = "PC MAC :";
+            // 
+            // lblSWver
+            // 
+            this.lblSWver.AutoSize = true;
+            this.lblSWver.ForeColor = System.Drawing.Color.Red;
+            this.lblSWver.Location = new System.Drawing.Point(13, 399);
+            this.lblSWver.Name = "lblSWver";
+            this.lblSWver.Size = new System.Drawing.Size(83, 15);
+            this.lblSWver.TabIndex = 101;
+            this.lblSWver.Text = "SW_ver_date";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 55);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 341);
+            this.tabControl1.TabIndex = 104;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtOcrResult);
+            this.tabPage1.Controls.Add(this.lblOcrResult);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.btnImgRead);
+            this.tabPage1.Controls.Add(this.FilePath1);
+            this.tabPage1.Controls.Add(this.btnImgSelect);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 312);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabAOI";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 312);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnImgRead
+            // 
+            this.btnImgRead.Location = new System.Drawing.Point(624, 18);
+            this.btnImgRead.Name = "btnImgRead";
+            this.btnImgRead.Size = new System.Drawing.Size(138, 40);
+            this.btnImgRead.TabIndex = 111;
+            this.btnImgRead.Text = "Read image";
+            this.btnImgRead.UseVisualStyleBackColor = true;
+            this.btnImgRead.Click += new System.EventHandler(this.btnImgRead_Click);
+            // 
+            // FilePath1
+            // 
+            this.FilePath1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FilePath1.Location = new System.Drawing.Point(150, 29);
+            this.FilePath1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.FilePath1.Name = "FilePath1";
+            this.FilePath1.Size = new System.Drawing.Size(468, 25);
+            this.FilePath1.TabIndex = 110;
+            // 
+            // btnImgSelect
+            // 
+            this.btnImgSelect.Location = new System.Drawing.Point(6, 18);
+            this.btnImgSelect.Name = "btnImgSelect";
+            this.btnImgSelect.Size = new System.Drawing.Size(138, 40);
+            this.btnImgSelect.TabIndex = 109;
+            this.btnImgSelect.Text = "SelectImage";
+            this.btnImgSelect.UseVisualStyleBackColor = true;
+            this.btnImgSelect.Click += new System.EventHandler(this.btnImgSelect_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(468, 247);
+            this.pictureBox1.TabIndex = 112;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblOcrResult
+            // 
+            this.lblOcrResult.AutoSize = true;
+            this.lblOcrResult.Font = new System.Drawing.Font("Century", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOcrResult.Location = new System.Drawing.Point(480, 81);
+            this.lblOcrResult.Name = "lblOcrResult";
+            this.lblOcrResult.Size = new System.Drawing.Size(104, 22);
+            this.lblOcrResult.TabIndex = 113;
+            this.lblOcrResult.Text = "OCR result";
+            // 
+            // txtOcrResult
+            // 
+            this.txtOcrResult.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtOcrResult.Location = new System.Drawing.Point(484, 193);
+            this.txtOcrResult.Name = "txtOcrResult";
+            this.txtOcrResult.Size = new System.Drawing.Size(278, 31);
+            this.txtOcrResult.TabIndex = 114;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.txtMac);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lblSWver);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox txtMac;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lblSWver;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnImgRead;
+        private System.Windows.Forms.TextBox FilePath1;
+        private System.Windows.Forms.Button btnImgSelect;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblOcrResult;
+        private System.Windows.Forms.TextBox txtOcrResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿/*****************************************************************************
+ **
+ **  (c) All Rights Reserved.
+ **
+ ** 2025-09-15, 版本1.0.0.0： WinFormsApp_PicRead，讀取圖檔，使用 Tesseract OCR 引擎 進行文字辨識
+ **
+ **
+******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;    //使用 Tesseract OCR 引擎 進行文字辨識
+using TesseractOCR;
+
+namespace WinFormsApp_PicRead
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _selectedImage;  // 用於存儲選擇的圖片
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*******************************************************************************
+         * Constant 
+         ******************************************************************************/
+
+        /* These are constant variables */
+        public static readonly string userDocFolder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        /*******************************************************************************
+         * Gobal Variable 
+         ******************************************************************************/
+        static string userAppFolder = userDocFolder + "\\pxToRGB";
+        // 可選：獲取完整路徑
+        string executablePath = Assembly.GetExecutingAssembly().Location;
+        // 獲取目前執行檔的名稱
+        static string fileName = Assembly.GetExecutingAssembly().GetName().Name;
+        //獲取執行檔版本
+        string fileVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        string buildDateTime = System.IO.File.GetCreationTime(Assembly.GetExecutingAssembly().Location).ToString("yyyy-MM-dd HH:mm");    //ToString("yyyy-MM-dd HH:mm:ss")
+
+        string strFilePath = "";    //userAppFolder + "\\" + "H4_IQC_LBS_BURN.csv";
+        static string strDailyLogDate = DateTime.Now.ToString("yyyy-MM-dd");
+        static string strFileDailyFolder = userAppFolder + "\\" + DateTime.Now.ToString("yyyy")
+            + "\\" + DateTime.Now.ToString("MM") + "\\" + DateTime.Now.ToString("dd");
+        static string strLogNameDaily = ""; //strFileDailyFolder + "\\" + "H4_IQC_LBS_BURN" + "_" + strDailyLogDate + ".csv";
+        string strFileDailyPath = "";   //userAppFolder + "\\" + strLogNameDaily;    //@"\Burn1to10.csv";
+        string strSwVer = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion.ToString();
+
+        Boolean bRemoveTabpge2 = true; //預設顯示tabPage1
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtMac.Text = GetMacAddress().ToString();
+            lblSWver.Text = /*"SW : " +*/ fileName + " - ver: " + strSwVer + " ; 程式碼日期 : " + buildDateTime;
+
+            if (bRemoveTabpge2) //預設不顯示tabPage2，2025-09-08
+            {
+                tabControl1.TabPages.Remove(tabPage2);
+            }
+            btnImgRead.Enabled = false;
+
+            toolStripStatusLabel1.Text = "按 Select image 鈕選擇檔案";
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            utilHelpAbout();
+        }
+        private void utilHelpAbout()
+        {
+            string company = "Test App " + fileName + " Ver:" + fileVersion + "\n";
+            string copyright = "(C)Copyright Eric Hsu 2025-2035.\n";
+            string buildDT = "Built on " + buildDateTime + "\n";
+            string Author = "Author: Eric Hsu\n";
+            MessageBox.Show(company + copyright + buildDT + Author, "About " + fileName);
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        public static string GetMacAddress()
+        {
+            string macAddresses = "";
+
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet)  //2023-03-21
+                {
+                    macAddresses += nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+                else if (nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    macAddresses += nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+            }
+            macAddresses = AddSpaceEveryNChar(macAddresses, 2); //2025-06-19
+            return macAddresses;
+        }
+        private void utilCheckUserFile()
+        {
+            if (Directory.Exists(userAppFolder) == false)
+            { Directory.CreateDirectory(userAppFolder); }
+            //if (Directory.Exists(strFileDailyFolder) == false)
+            //{ Directory.CreateDirectory(strFileDailyFolder); }
+        }
+        public bool IsFileLocked(string filename)
+        {
+            bool Locked = false;
+            try
+            {
+                FileStream fs =
+                    File.Open(filename, FileMode.OpenOrCreate,
+                    FileAccess.ReadWrite, FileShare.None);
+                fs.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Locked = true;
+            }
+            return Locked;
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //Environment.Exit(Environment.ExitCode);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to exit？", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            { }
+            else { e.Cancel = true; }
+        }
+
+        static string AddSpaceEveryNChar(string str, int split)   //2023-08-11
+        {
+            for (int a = 2; a <= str.Length - 1; a = a + split + 1)
+            {
+                str = str.Insert(a, "-");
+            }
+            Console.WriteLine(str);
+            return str;
+        }
+
+        private void btnImgSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Title = "Select A File";
+            openDialog.Filter = "Image Files (*.png;*.jpg;*.bmp;*.gif)|*.png;*.jpg;*.bmp;*.gif" + "|" + "All Files (*.*)|*.*";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = openDialog.FileName;
+                FilePath1.Text = file;
+                toolStripStatusLabel1.Text = "InputFile= " + FilePath1.Text;
+                try
+                {
+                    _selectedImage = new Bitmap(openDialog.FileName);
+                    pictureBox1.Image = _selectedImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading image: " + ex.Message);
+                }
+            }
+            if (FilePath1.Text != "") { btnImgRead.Enabled = true; } 
+            else { btnImgRead.Enabled = false; }
+
+            toolStripStatusLabel1.Text = "可按下 Read image 按鈕以產出結果";
+        }
+
+        private void btnImgRead_Click(object sender, EventArgs e)
+        {
+            if (_selectedImage == null)
+            {
+                MessageBox.Show("Please load an image first.");
+                return;
+            }
+
+            try
+            {
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default)) // 確保 tessdata 資料夾存在，並包含 "eng.traineddata"
+                {
+                    // 建立一個 TesseractEngine 實例，指定 tessdata 資料夾的路徑和語言
+                    // "eng" 代表英文。 你可以根據需要更改語言 (例如 "chi_tra" 代表繁體中文)
+                    // 請確保你的 tessdata 資料夾中包含對應的語言檔 (.traineddata)
+                    var page = engine.Process(_selectedImage);
+                    string text = page.GetText();
+                    lblOcrResult.Text = text;
+                    lblOcrResult.Font = new Font("Arial", 16); // 設定字型和大小
+                    lblOcrResult.ForeColor = Color.Blue; // 設定字體顏色
+                    txtOcrResult.Text = text;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error recognizing text: " + ex.Message);
+            }
+        }
+    }
+}
